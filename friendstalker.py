914edcc48ad3a65d80d7931d0d236f60934eedc2
Bram Bonné <bram.bonne@uhasselt.be>
2013-11-28 17:01:08 +0100

    Update list on the fly, newest last.

--- friendstalker.py
+++ friendstalker.py
@@ -17,27 +17,27 @@ parser = ArgumentParser(description="Show your friends' last scrobbles on Last.f
                                  formatter_class=ArgumentDefaultsHelpFormatter)
 parser.add_argument('--username', '-u', dest='username', help='your Last.fm username', type=str, default='megooz')
 parser.add_argument('--history', dest='history_time', help='number of seconds ago to display scrobbles from', type=int, default=600)
-parser.add_argument('--max_tracks', '-t', dest='max_tracks', help='maximum number of tracks to display per friend', type=int, default=3)
+parser.add_argument('--max_tracks', '-t', dest='max_tracks', help='maximum number of tracks in history to display per friend', type=int, default=10)
 parser.add_argument('--run_indefinitely', action='store_true', dest='run_indefinitely', default=False)
 args = vars(parser.parse_args())
 
 network = pylast.get_lastfm_network(api_key=API_KEY, api_secret=API_SECRET)
 user = network.get_user(args['username'])
 friends = user.get_friends()
+prev_scrobbles = set()
 while args['run_indefinitely']: # Keep running if this switch is on
     scrobbles = []
     for friend in friends:
         tracks = friend.get_recent_tracks(args['max_tracks'])
         for played_track in tracks:
             # Only keep scrobble if it's recent enough
-            if int(played_track.timestamp) > time() - args['history_time']: # Less than 10 minutes ago
+            timestamp = int(played_track.timestamp)
+            if timestamp > time() - args['history_time'] and repr(played_track) not in prev_scrobbles:
+                prev_scrobbles.add(repr(played_track)) # Print every scrobble only once
                 scrobbles.append(Scrobble(played_track.timestamp, friend, played_track.track))
 
-    # Clear screen
-    clscommand = 'cls' if os.name=='nt' else 'clear'
-    os.system(clscommand)
     # Print all recent scrobbles
-    for scrobble in sorted(scrobbles, key=lambda s: s.timestamp, reverse=True):
+    for scrobble in sorted(scrobbles, key=lambda s: s.timestamp): # Sort them by timestamp
         friendname = scrobble.user.get_name()
         artist = scrobble.track.get_artist()
         title = scrobble.track.get_title()
